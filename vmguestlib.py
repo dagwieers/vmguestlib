d3d1f51d6afa81ec1d9eabf9bf0d5514f512e233
Dag Wieers <dag@wieers.com>
2013-12-09 16:45:40 +0100

    Add library metadata and GPL license header

--- vmguestlib.py
+++ vmguestlib.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python
 
+### This program is free software; you can redistribute it and/or
+### modify it under the terms of the GNU General Public License
+### as published by the Free Software Foundation; either version 2
+### of the License, or (at your option) any later version.
+###
+### This program is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+### GNU General Public License for more details.
+###
+### You should have received a copy of the GNU General Public License
+### along with this program; if not, write to the Free Software
+### Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+### Copyright 2013 Dag Wieers <dag@wieers.com>
+
 from ctypes import *
 
-__version__ = '0.1.0'
+__author__ = 'Dag Wieers <dag@wieers.com>'
+__version__ = '0.1.1'
+__version_info__ = tuple([ int(d) for d in __version__.split('.') ])
+__license__ = 'GNU General Public License (GPL)'
 
 vmGuestLib = cdll.LoadLibrary('/usr/lib/vmware-tools/lib/libvmGuestLib.so/libvmGuestLib.so')
 
@@ -301,3 +320,5 @@ class VMGuestLib(Structure):
         ret = vmGuestLib.VMGuestLib_GetMemUsedMB(self.handle.value, byref(counter))
         if ret != VMGUESTLIB_ERROR_SUCCESS: raise VMGuestLibException(ret)
         return counter.value
+
+# vim:ts=4:sw=4:et
